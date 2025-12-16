9742cb06d6222c95302abbdf02c04285a4e2949c
surajkanse88 <surajkanse88@gmail.com>
2025-12-16 17:59:22 +0530

    Reintroduce extraProp and log array properties
    Reintroduce extraProp to Array prototype and log original properties.

--- JustRandom.js
+++ JustRandom.js
@@ -1,9 +1,17 @@
 
 // loop in the original properties of the array
 
-Array.prototype.extraProp = "SURAJ"
 
-let newarr = [10, 20, 30, 40, 50]
+// there are some additional properties inserted by using the protype of array
+// ignore the properties injected by prototyping
+// and loop in tyhe original properties of the array
+
+Array.prototype.extraProp = "SURAJ";
+
+let newarr = [10, 20, 30, 40, 50];
 
 for (let v in newarr)
+{
+  console.log(v);
+}
 
